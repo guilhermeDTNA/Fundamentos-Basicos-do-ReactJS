7ccfa287c0cd5c108269998bef0251f21a381aa0
guilherme <gleite98@gmail.com>
2020-06-12 18:34:41 -0300

    Working with states and components's life cycle

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React, {Component} from 'react';
 
-//Para constante é necessário passar as props por referência
-const Bemvindo = (props) => {
+//To constants is necessary send the props through reference
+const Welcome = (props) => {
 
 	return(
 		<div>
-		<h2>Bem-vindo, {props.nome}, tenho {props.idade} anos</h2>
+		<h2>Welcome, {props.name}, you have {props.age} years old.</h2>
 		</div>
 		);
 }
 
-//Para classes é possível acessar as props só pelo "this", não precisa de referência
-class Equipe extends Component{
+//To classes it is possible view the props just by "this", it doesn't need reference
+class Staff extends Component{
 	render(){
 		return(
 
 			<div>
-			<Sobre nome={this.props.nome} cargo={this.props.cargo} idade={this.props.idade} />
+			<About name={this.props.name} office={this.props.office} age={this.props.age} />
 
 			<Social facebook={this.props.facebook} />
 			</div>
@@ -37,32 +37,92 @@ class Social extends Component{
 	}
 }
 
-class Sobre extends Component{
+class About extends Component{
 	render(){
 		return(
 			<div>
-			<h2>Olá sou o(a) {this.props.nome} </h2>
-			<h2>Cargo: {this.props.cargo}</h2>
-			<h2>Idade: {this.props.idade}</h2>
+			<h2>Hello, I'm {this.props.name} </h2>
+			<h2>Office: {this.props.office}</h2>
+			<h2>Age: {this.props.age}</h2>
 
 			<hr/>
 			</div>
-		);
+			);
 	}
 } 
 
-function App(){
-	return (
-		<div>
-		<Bemvindo nome="Guilherme" idade="22" />
+class App extends Component{
 
-		<h1>Conheça nossa equipe: </h1>
-		<Equipe nome="Lucas" cargo="Programmer" idade="29 anos" facebook="https://www.facebook.com/guilherme.rochaleite.9" />
-		<Equipe nome="Daniel" cargo="Enginner" idade="20 anos" facebook="https://www.facebook.com/guilherme.rochaleite.9"/>
+	constructor(props){
+		super(props);
+		this.state={
+			name: 'Teste',
+			counter: 0,
+			time: '00:00:00'
+		}
 
-		</div>
-		);
-}
+		this.increase = this.increase.bind(this);
+		this.decrease = this.decrease.bind(this);
+	}
+
+//this function is executed only one time 
+	componentDidMount(){
+		setInterval(()=>{
+			this.setState({time: new Date().toLocaleTimeString() })
+		});
+	}
+
+	//This function is called always that a state is updated
+	componentDidUpdate(){
+		//console.log('Some state was updated');
+	}
 
 
-export default App;
+	increase(){
+		let state = this.state;
+		state.counter++;
+		this.setState(state);
+	}
+
+	decrease(){
+		let state = this.state;
+
+		if(state.counter === 0){
+			alert("Unfortunately the counter assumed zero value, then it's impossible to continue");
+			return;
+		}
+
+		state.counter--;
+		this.setState(state);
+	}
+
+	render(){
+		return (
+			<div>
+			<div>
+			<Welcome name="Guilherme" age="22" />
+
+			<h1>Meet our staff: </h1>
+			<Staff name={this.state.name} office="Programmer" age="29 years old" facebook="https://www.facebook.com/guilherme.rochaleite.9" />
+			<Staff name="Daniel" office="Enginner" age="20 years old" facebook="https://www.facebook.com/guilherme.rochaleite.9"/>
+
+			</div>
+			<div>
+			<h2>Counter</h2>
+			<h3>
+			<button onClick={this.decrease}>-</button>
+			{this.state.counter}
+			<button onClick={this.increase}>+</button>
+			</h3>
+			</div>
+
+			<div>
+				{this.state.time}
+			</div>
+
+			</div>
+			);
+	}}
+
+
+	export default App;
